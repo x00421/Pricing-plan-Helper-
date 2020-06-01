9bc9f8b841004dd20a8bb9329abdae22372cc222
x00421 <60685051+x00421@users.noreply.github.com>
2020-06-01 17:26:16 +0700

    Create void function for mainMenu

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void mainMenu(); //функция для основного меню
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    mainMenu(); //для вывода начального меню в косоль
+    return 0;
+}
