39cf9e4c2f9fea1ef4d4b8ba9c3c2155a94aa9b4
x00421 <60685051+x00421@users.noreply.github.com>
2020-06-03 23:05:55 +0700

    create menu file and enable it, change makefile

--- makefile
+++ makefile
@@ -1,6 +1,9 @@
-Pricing_plan_Helper:./build/src/main.o
+Pricing_plan_Helper:./build/src/main.o ./build/src/Menu.o
 	g++ -std=c++17  ./build/src/main.o -o ./bin/Pricing_plan_Helper
 
 
 ./build/src/main.o: ./src/main.cpp
 	g++ -std=c++17 -c ./src/main.cpp -o ./build/src/main.o -lm
+
+./build/src/Menu.o: ./src/Menu.cpp
+	g++ -std=c++17 -c ./src/Menu.cpp -o ./build/src/Menu.o -lm
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "Menu.hpp"
 
 using namespace std;
 
@@ -61,62 +62,6 @@ int main()
     return 0;
 }
 
-void mainMenu()
-{
-    //вывод пунктов меню
-    system("cls");
-    setlocale(LC_ALL, "Russian");
-
-    cout << "\t       Черноколпаков, Дудинский, Кичигин - ИП-915" << endl;
-
-    cout << "\t1-для запуска ассистента!\n"
-         << "\t2-инструкция.\n"
-         << "\t3-сведенья об операторах.\n"
-         << "\t4-Выход.\n";
-
-    bool y = 0;
-
-    int choice; //для выбора пункта меню
-    while (!y) {
-        cout << ">";
-        cin >> choice;
-
-        if (choice < 1 || choice > 4)
-
-            cout << "Ошибка,такого пункта не существует!" << endl;
-        else
-            y = 1;
-    }
-
-    switch (choice) {
-    case 1:
-        question1();
-        break;
-    case 2:
-
-        system("cls");
-
-        cout << "\nДля запуска ассистента, помогающего выбрать тариф нажмите - "
-                "1\n"
-             << "Чтобы узнать информацию об операторах нажмите - 3 \n"
-             << "Курсовая работа была создана: Черноколпаков, Дудинский, "
-                "Кичигин - "
-                "ИП-915\n"
-             << "\n"
-             << endl;
-        system("pause");
-        mainMenu();
-        break;
-    case 3:
-
-        system("cls");
-        ob_operatorax();
-        break;
-
-    case 4:
-        exit(0); //выход
-    }
-}
 
 void ob_operatorax()
 {
